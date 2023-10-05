366025a95192cd3a5951c5c535b5ccbbde774696
drewbomar <drewbomar@localhost>
2023-10-05 12:50:47 -0500

    PICKLES!!!!!!!!!!!!

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,69 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+
+		int numShapes = in.nextInt();
+		in.nextLine();
+		
+		for(int i = 0; i<numShapes; i++) {
+			
+		String shape = in.next();	
+		
+		int redComp = in.nextInt();
+		int greenComp = in.nextInt();
+		int blueComp = in.nextInt();
+		
+		boolean isFilled = in.nextBoolean();
+
+		double param1 = in.nextDouble();
+		double param2 = in.nextDouble();
+		double param3 = in.nextDouble();
+		double param4 = in.nextDouble();
+		
+		StdDraw.setPenColor(redComp, greenComp, blueComp);
+		
+		
+		if(isFilled)
+		StdDraw.filledRectangle(param1, param2, param3, param4);
+		else
+		StdDraw.rectangle(param1, param2, param3, param4);
+	
+		
+		in.nextLine();
+		StdDraw.show();
+		
+		}
+		
+		int numCircles = in.nextInt();
+		in.nextLine();
+		
+		for(int i=0; i<numCircles; i++) {
+			String shape = in.next();
+			
+			int redComp = in.nextInt();
+			int greenComp = in.nextInt();
+			int blueComp = in.nextInt();
+			
+			boolean isFilled = in.nextBoolean();
+
+			double param1 = in.nextDouble();
+			double param2 = in.nextDouble();
+			double param3 = in.nextDouble();
+			
+			StdDraw.setPenColor(redComp, greenComp, blueComp);
+			
+			
+			if(isFilled)
+			StdDraw.filledCircle(param1, param2, param3);
+			else
+			StdDraw.circle(param1, param2, param3);
+			
+			in.nextLine();
+			StdDraw.show();
+		}
+		
+		
 		
 	}
 }
